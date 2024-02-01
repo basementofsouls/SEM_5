9696550111b71d95e75414a6b5c72f06cc618f4d
Stacy <nat.kkurag.0301@mail.ru>
2024-02-01 23:55:52 +0300

    OS

---
+++ ОС/Lab5/Lab5/Lab5.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <thread>
+
+int main() {
+    // Устанавливаем кодировку для ввода и вывода в русскую (Windows-1251)
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // Идентификатор текущего процесса
+    DWORD currentProcessId = GetCurrentProcessId();
+    std::cout << "Идентификатор текущего процесса: " << currentProcessId << std::endl;
+
+    // Идентификатор текущего (main) потока
+    DWORD currentThreadId = GetCurrentThreadId();
+    std::cout << "Идентификатор текущего (main) потока: " << currentThreadId << std::endl;
+
+    // Приоритет (приоритетный класс) текущего процесса
+    int currentProcessPriority = GetPriorityClass(GetCurrentProcess());
+    std::cout << "Приоритет текущего процесса: " << currentProcessPriority << std::endl;
+
+    // Приоритет текущего потока
+    int currentThreadPriority = GetThreadPriority(GetCurrentThread());
+    std::cout << "Приоритет текущего потока: " << currentThreadPriority << std::endl;
+    SetProcessAffinityMask(GetCurrentProcess(), 3);
+    // Маска доступных процессу процессоров в двоичном виде
+    DWORD_PTR processAffinityMask, systemAffinityMask;
+    if (GetProcessAffinityMask(GetCurrentProcess(), &processAffinityMask, &systemAffinityMask)) {
+        std::cout << "Маска (affinity mask) доступных процессу процессоров в двоичном виде: " << processAffinityMask << std::endl;
+    }
+
+   
+    int count = 0;
+    while (processAffinityMask) {
+        count += processAffinityMask & 1; 
+        processAffinityMask >>= 1; 
+    }
+    // Количество процессоров доступных процессу
+    int numberOfProcessors = count;
+    std::cout << "Количество процессоров доступных процессу: " << numberOfProcessors << std::endl;
+
+    // Процессор, назначенный текущему потоку
+    int currentThreadProcessor = GetCurrentProcessorNumber();
+    std::cout << "Процессор, назначенный текущему потоку: " << currentThreadProcessor << std::endl;
+
+    return 0;
+}
---
+++ ОС/Lab5/OS05_02x/OS05_02x.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+    // Устанавливаем кодировку для ввода и вывода в русскую (Windows-1251)
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // Вывод информации (номер итерации, идентификаторы и т.д.)
+    DWORD processId = GetCurrentProcessId();
+    DWORD threadId = GetCurrentThreadId();
+
+    
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    // Пример установки цвета текста
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+
+    for (int iteration = 1; iteration <= 1000000; ++iteration) {
+        if (iteration % 1000 == 0) {
+            Sleep(200);
+            std::cout << "Итерация: "               << iteration
+                << " | ID процесса: "               << processId
+                << " | ID потока: "                 << threadId
+                << " | Приоритет процесса: "        << GetPriorityClass(GetCurrentProcess())
+                << " | Приоритет потока: "          << GetThreadPriority(GetCurrentThread())
+                << " | Назначенный процессор: "     << GetCurrentProcessorNumber() << std::endl;
+        }
+    }
+    char a;
+    std::cin >> a;
+    return 0;
+}
