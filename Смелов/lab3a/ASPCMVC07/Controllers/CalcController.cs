10f2ccdc464a794eed272311448729b1653a8935
Stacy <nat.kkurag.0301@mail.ru>
2024-02-02 00:03:12 +0300

    strwp

---
+++ Смелов/lab3a/ASPCMVC07/Controllers/CalcController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCMVC07.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.press = "+";
+            return View("Calc");
+        }
+
+
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Sum(float? x, float? y)
+        {
+            if (x == null || y == null)
+            {
+                ViewBag.Error = "--ERROR: Invalid input!";
+                ViewBag.press = "+";
+                return View("Calc");
+            }
+            ViewBag.x = x;
+            ViewBag.y = y;
+            ViewBag.z = x + y;
+            ViewBag.press = "+";
+           
+            return View("Calc");
+            
+        }
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Sub(float? x, float? y)
+        {
+            if (x == null || y == null)
+            {
+                ViewBag.Error = "--ERROR: Invalid input!";
+                ViewBag.press = "-";
+                return View("Calc");
+            }
+
+            ViewBag.x = x;
+            ViewBag.y = y;
+            ViewBag.z = x - y;
+            ViewBag.press = "-";
+            return View("Calc");
+        }
+
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Mul(float? x, float? y)
+        {
+            if (x == null || y == null)
+            {
+                ViewBag.Error = "--ERROR: Invalid input!";
+                ViewBag.press = "*";
+                return View("Calc");
+            }
+
+            ViewBag.x = x;
+            ViewBag.y = y;
+            ViewBag.z = x * y;
+            ViewBag.press = "*";
+            return View("Calc");
+        }
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Div(float? x, float? y)
+        {
+            
+            if (x == null || y == null)
+            {
+                ViewBag.Error = "--ERROR: Invalid input!";
+                ViewBag.press = "/";
+                return View("Calc");
+            }
+
+            ViewBag.x = x;
+            ViewBag.y = y;
+            ViewBag.z = x / y;
+            ViewBag.press = "/";
+            return View("Calc");
+        }
+    }
+
+}
