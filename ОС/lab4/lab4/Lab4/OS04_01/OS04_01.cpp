2c55bd8853b127a177209466a926b42b42818a26
Stacy <nat.kkurag.0301@mail.ru>
2024-02-01 23:54:52 +0300

    OS

---
+++ ОС/lab4/lab4/Lab4/OS04_01/OS04_01.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+// 1. Powershell ISE:		(Get-Process OS04_01).Threads
+// 2. Performance Monitor
+
+int main()
+{
+	DWORD pid = GetCurrentProcessId();
+	DWORD tid = GetCurrentThreadId();
+
+	for (short i = 1; i <= 100; ++i)
+	{
+		cout << i << ". PID = " << pid << "\t\tTID = " << tid << "\n";
+		Sleep(1000);
+	}
+
+	system("pause");
+	return 0;
+}
